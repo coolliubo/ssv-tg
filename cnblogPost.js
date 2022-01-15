c3cdc1a06b7b56b18383b4fbbd916d21f6bd3d76
coolliubo <coolliubo@gmail.com>
2022-01-15 09:37:35 +0800

    fix bugs  20220115 093735

--- cnblogPost.js
+++ cnblogPost.js
@@ -1,12 +1,12 @@
 const fs = require("fs")
 //const sqlite = require('./asqlite3.js')
-//const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer')
 const core = require('@actions/core')
 const github = require('@actions/github')
-const puppeteer = require('puppeteer-extra')
+/*const puppeteer = require('puppeteer-extra')
 // add stealth plugin and use defaults (all evasion techniques)
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
+puppeteer.use(StealthPlugin())*/
 const { tFormat, sleep, clearBrowser, getRndInteger, randomOne, randomString, md5, waitForString, findFrames  } = require('./common.js')
 const { changeContent, cutStrin, filterContent } = require('./utils.js')
 Date.prototype.format = tFormat
@@ -80,7 +80,7 @@ async function postArticles(row, page) {
 async function main() {
     browser = await puppeteer.launch({
         headless: runId ? true : false,
-        //headless: true,
+        headless: true,
     args: [
       '--window-size=1920,1080',
       '--no-sandbox',
