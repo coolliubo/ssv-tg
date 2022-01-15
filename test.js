c2d40d2828eeec0581922c51a01f56bc44741d2f
coolliubo <coolliubo@gmail.com>
2022-01-15 21:59:10 +0800

    fix bugs  20220115 215910

--- test.js
+++ test.js
@@ -1,15 +1,34 @@
-// puppeteer-extra is a drop-in replacement for puppeteer,
-// it augments the installed puppeteer with plugin functionality
+const fs = require("fs")
+//const sqlite = require('./asqlite3.js')
+//const puppeteer = require('puppeteer')
+const core = require('@actions/core')
+const github = require('@actions/github')
 const puppeteer = require('puppeteer-extra')
-
 // add stealth plugin and use defaults (all evasion techniques)
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
-
+const runId = github.context.runId
+let setup = {}
+if (!runId) {
+    setup = JSON.parse(fs.readFileSync('./setup.json', 'utf8'))
+} else {
+    setup = JSON.parse(process.env.SETUP)
+}
 // puppeteer usage as normal
-puppeteer.launch({ headless: true }).then(async browser => {
+puppeteer.launch({ 
+    headless: runId ? true : false,
+    //headless: true,
+    args: [
+        '--window-size=1920,1080',
+        setup.proxy.normal
+    ],
+    defaultViewport: null,
+    ignoreHTTPSErrors: true,
+    
+}).then(async browser => {
   console.log('Running tests..')
   const page = await browser.newPage()
+  await page.authenticate({username:setup.proxy.usr, password:setup.proxy.pwd});
   await page.goto('https://bot.sannysoft.com')
   await page.waitForTimeout(5000)
   await page.screenshot({ path: 'testresult.png', fullPage: true })
