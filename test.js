28f9df616dd7356ad5ec5f3a45a77bf798794116
coolliubo <coolliubo@gmail.com>
2022-01-16 09:26:32 +0800

    fix bugs  20220116 092632

--- test.js
+++ test.js
@@ -32,7 +32,22 @@ puppeteer.launch({
   let cookies = JSON.parse(fs.readFileSync('./cnblog.json', 'utf8'))
   await page.setCookie(...cookies)
   await page.goto('https://www.cnblogs.com/eroslp/')
-  await page.waitForTimeout(5000)
+  await page.waitForTimeout(500)
+  let selecter = '#Header1_HeaderTitle'
+  await page.waitForSelector(selecter, { timeout: 5000 })
+  .catch(async (error)=>{
+      console.log(await page.$eval('body', el => el.innerText))
+  })
+  await page.waitForTimeout(1000)
+  await page.goto('https://i.cnblogs.com/posts/edit')
+  await page.waitForTimeout(500)
+  selecter ='#post-title'
+  await page.waitForSelector(selecter)
+  .then(async () =>console.log('找到#post-title') )
+  //await page.evaluate((selecter, text) => document.querySelector(selecter).value = text, '#txtTitle', row.title)
+  await page.type(selecter,'破解下载')
+  
+  await page.waitForTimeout(3000)
   await page.screenshot({ path: 'testresult.png', fullPage: true })
   await browser.close()
   console.log(`All done, check the screenshot. ✨`)
