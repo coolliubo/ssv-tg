b5012983af24942b49dda7062aece6353eda7cb3
coolliubo <coolliubo@gmail.com>
2022-01-16 09:11:58 +0800

    fix bugs  20220116 091158

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ puppeteer.launch({
   console.log('Running tests..')
   const page = await browser.newPage()
   await page.authenticate({username:setup.proxy.usr, password:setup.proxy.pwd});
-  await page.goto('https://www.cnblogs.com/eroslp/')
+  await page.goto('https://bot.sannysoft.com')
   await page.waitForTimeout(5000)
   await page.screenshot({ path: 'testresult.png', fullPage: true })
   await browser.close()
