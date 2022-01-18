09516dfe2d0108597b7ec148ba5d23164d220907
coolliubo <coolliubo@gmail.com>
2022-01-18 11:03:13 +0800

    fix bugs  20220118 110313

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const puppeteer = require('puppeteer')
 const core = require('@actions/core')
 const github = require('@actions/github')
-const proxyChain = require('proxy-chain')
+//const proxyChain = require('proxy-chain')
 // change username & password
 /* const puppeteer = require('puppeteer-extra')
 // add stealth plugin and use defaults (all evasion techniques)
@@ -33,15 +33,15 @@ const pool = mysql.createPool({
 })
 
 async function main() {
-    const oldProxyUrl = 'http://app.aiboboxx.ml:7799'
-    const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
+    //const oldProxyUrl = 'http://193.164.131.202:7890'
+    //const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
     const browser = await puppeteer.launch({ 
         headless: runId ? true : false,
         //headless: true,
         args: [
             '--window-size=1920,1080',
-            //'--proxy-server=socks5://app.aiboboxx.ml:7799',
-            `--proxy-server=${newProxyUrl}`,
+            '--proxy-server=socks5://39.165.98.152:7302',
+            //`--proxy-server=${newProxyUrl}`,
             '--ignore-certificate-errors',
             '--ignore-certificate-errors-spki-list '
         ],
@@ -57,7 +57,8 @@ async function main() {
     //await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36')
     let cookies = JSON.parse(fs.readFileSync(ckfile, 'utf8'))
     await page.setCookie(...cookies)
-    await page.goto('https://www.youtube.com/watch?v=Yo_VyP7qlC8&ab_channel=KKBOX%E8%8F%AF%E8%AA%9E%E6%96%B0%E6%AD%8C-kkboxmusic')
+    //await page.goto('https://www.youtube.com/watch?v=Yo_VyP7qlC8&ab_channel=KKBOX%E8%8F%AF%E8%AA%9E%E6%96%B0%E6%AD%8C-kkboxmusic')
+    await page.goto('https://en.ipip.net/')
     await page.waitForTimeout(3000)
     let selecter = '#Header1_HeaderTitle'
     await page.waitForSelector(selecter, { timeout: 5000 })
